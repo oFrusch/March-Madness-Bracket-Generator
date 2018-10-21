e28ce651b09f1c749d3434568bfd7f2cb357427e
unknown <owenc343@gmail.com>
2018-10-21 14:58:27 -0400

    Randomization is done better. Still need to find best range

--- venv/Bracket.py
+++ venv/Bracket.py
@@ -48,11 +48,11 @@ sweet_sixteen = {
 
 
 # Generates all the teams for one conference
-def generate_teams():
+def generate_teams(x):
     initial_queue = queue.Queue(maxsize=16)
     for i, j in sweet_sixteen.items():
-        team1 = make_team(i, "team " + str(i))
-        team2 = make_team(j, "team " + str(i))
+        team1 = make_team(i, "team " + x + str(i))
+        team2 = make_team(j, "team " + x + str(i))
         initial_queue.put(team1)
         initial_queue.put(team2)
     return initial_queue
@@ -63,10 +63,11 @@ def simulate_round1(teams):
     round2 = queue.Queue(maxsize=8)
     while not teams.empty():
         random_num = random.randint(0, 5) / 5
+        random_num2 = random.randint(0, 5) / 5
         team1 = teams.get()
         team2 = teams.get()
         team1_chance = team1.round1_chance * random_num
-        team2_chance = team2.round1_chance * random_num
+        team2_chance = team2.round1_chance * random_num2
         add_team = team1 if team1_chance > team2_chance else team2
         round2.put(add_team)
     return round2
@@ -77,10 +78,11 @@ def simulate_round2(teams):
     sweet_sixteen_queue = queue.Queue(maxsize=4)
     while not teams.empty():
         random_num = random.randint(0, 5) / 5
+        random_num2 = random.randint(0, 5) / 5
         team1 = teams.get()
         team2 = teams.get()
         team1_chance = team1.sweet_sixteen_chance * random_num
-        team2_chance = team2.sweet_sixteen_chance * random_num
+        team2_chance = team2.sweet_sixteen_chance * random_num2
         add_team = team1 if team1_chance > team2_chance else team2
         sweet_sixteen_queue.put(add_team)
     return sweet_sixteen_queue
@@ -91,10 +93,11 @@ def simulate_16(teams):
     elite_eight_queue = queue.Queue(maxsize=2)
     while not teams.empty():
         random_num = random.randint(0, 5) / 5
+        random_num2 = random.randint(0, 5) / 5
         team1 = teams.get()
         team2 = teams.get()
         team1_chance = team1.elite_eight_chance * random_num
-        team2_chance = team2.elite_eight_chance * random_num
+        team2_chance = team2.elite_eight_chance * random_num2
         add_team = team1 if team1_chance > team2_chance else team2
         elite_eight_queue.put(add_team)
     return elite_eight_queue
@@ -103,15 +106,16 @@ def simulate_16(teams):
 # Returns the team from one region that will go to championship
 def simulate_8(teams):
     random_num = random.randint(0, 5) / 5
+    random_num2 = random.randint(0, 5) / 5
     team1 = teams.get()
     team2 = teams.get()
     team1_chance = team1.final_four_chance * random_num
-    team2_chance = team2.final_four_chance * random_num
+    team2_chance = team2.final_four_chance * random_num2
     return team1 if team1_chance > team2_chance else team2
 
 
-def simulate_conference():
-    teams = generate_teams()
+def simulate_conference(x):
+    teams = generate_teams(x)
     teams2 = simulate_round1(teams)
     teams16 = simulate_round2(teams2)
     teams8 = simulate_16(teams16)
@@ -119,28 +123,38 @@ def simulate_conference():
     return conference_winner
 
 
-def simulate_region():
-    team1 = simulate_conference()
-    team2 = simulate_conference()
+def simulate_region(x, y):
+    team1 = simulate_conference(x)
+    team2 = simulate_conference(y)
     random_num = random.randint(0, 5) / 5
+    random_num2 = random.randint(0, 5) / 5
     team1_chance = team1.champ_game_chance * random_num
-    team2_chance = team2.champ_game_chance * random_num
+    team2_chance = team2.champ_game_chance * random_num2
     return team1 if team1_chance > team2_chance else team2
 
 
-def simulate_tournament():
-    team1 = simulate_region()
-    team2 = simulate_region()
+def simulate_tournament(x, y, z, w):
+    team1 = simulate_region(x, y)
+    team2 = simulate_region(z, w)
     random_num = random.randint(0, 5) / 5
+    random_num2 = random.randint(0, 5) / 5
     team1_chance = team1.win_chance * random_num
-    team2_chance = team2.win_chance * random_num
-    return team1 if team1_chance > team2_chance else team2
+    team2_chance = team2.win_chance * random_num2
+    return team1 if team1_chance >= team2_chance else team2
 
 
 winners = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0}
-for i in range(0, 100000):
-    winners[simulate_tournament().seed] += 1
+winner_names = []
+for i in range(0, 1000):
+    winner = simulate_tournament("1", "2", "3", "4")
+    winner_names.append(winner.name)
+    winners[winner.seed] += 1
+
+
+for j in winner_names:
+    print(j)
+
 
 for i in winners:
-    print(i)
+    print("Seed " + str(i) + ": " + str(winners[i]) + " Percent Win: " + str(winners[i]/1000))
 
